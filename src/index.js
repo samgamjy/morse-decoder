dbcc89769623698adbcbf7f9953b464403f575df
miklevasilevich <miklevasilevich@gmail.com>
2021-08-21 22:15:57 +0300

    feat: add function for decode message

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function getLetter(str) {
+    let resMsg = '';
+    for (let i = 0; i < (str.length / 2); i++) {
+        let item = '';
+        item = (str[i*2] + str[i*2 + 1]);
+        if (item === '10') {
+            resMsg += '.';
+        } else if (item === '11') {
+            resMsg += '-';
+        }            
+    }
+    return resMsg;
+}
+
 function decode(expr) {
-    // write your solution here
+    console.log(expr);
+    
+    let res = '';
+    let map = new Map(Object.entries(MORSE_TABLE));
+    let resMsg = '';
+
+    for (let i = 0; i < (expr.length / 10); i++) {        
+        res =  expr.slice(i * 10, (i + 1) * 10);
+        if (res === '**********') {
+            resMsg += ' ';
+        }else {
+            resMsg += map.get(getLetter(res));
+        }
+    }
+    return resMsg;    
 }
 
 module.exports = {
